e19e4ba34049198bb58f63540413241302df979f
datisekai <84700751+datisekai@users.noreply.github.com>
2022-03-08 19:02:15 +0700

    first commit

---
+++ src/components/Ticket.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+const Ticket = ({
+  ticket,
+  handleMove,
+  ticketKey,
+  handleDelete,
+  handleEdit,
+}) => {
+  const buttonArray = [
+    { todos: "todos", key: "todos", id: uuidv4() },
+    { todos: "in-progress", key: "inProgress", id: uuidv4() },
+    { todos: "done", key: "done", id: uuidv4() },
+    { todos: "need-review", key: "needReview", id: uuidv4() },
+    { todos: "delete", key: "delete", id: uuidv4() },
+    { todos: "edit", key: "edit", id: uuidv4() },
+  ];
+  const buttonRender =
+    ticket &&
+    buttonArray.filter((item) => item.todos !== ticket.currentColumnKey);
+
+  const [flagText, setFlagText] = useState(false);
+  const [text, setText] = useState("");
+  return (
+    <div className="border-b border-blue-400 py-2 mt-2">
+      <p className="text-gray-100 uppercase">
+        id: <span className="text-blue-400">{ticket.id}</span>
+      </p>
+      {!flagText ? (
+        <p className="text-gray-100 uppercase">
+          Text: <span className="text-blue-400">{ticket.text}</span>
+        </p>
+      ) : (
+        <div className="flex justify-between items-center">
+          <form>
+            <input
+              className="bg-transparent border-b px-4 py-1 w-[70%] border-blue-400 outline-none text-gray-100"
+              placeholder="Enter your new text.."
+              type="textarea"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            ></input>
+            <button
+              type="submit"
+              onClick={(e) => {
+                setFlagText(false);
+                return handleEdit(ticketKey, ticket.id, text, e);
+              }}
+              className="bg-blue-400 px-4 py-1 w-[30%] text-gray-100 hover:bg-blue-700 transition-all rounded-md"
+            >
+              Edit
+            </button>
+          </form>
+        </div>
+      )}
+      <p className="text-gray-100 uppercase">
+        createdDate: <span className="text-blue-400">{ticket.createdDate}</span>
+      </p>
+      {ticket.lastModifiedDate && (
+        <p className="text-gray-100 uppercase">
+          lastModifiedDate:{" "}
+          <span className="text-blue-400">{ticket.createdDate}</span>
+        </p>
+      )}
+      <p className="text-gray-100 uppercase">
+        currentColumnKey:{" "}
+        <span className="text-blue-400">{ticket.currentColumnKey}</span>
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 mt-2">
+        {buttonRender &&
+          buttonRender.map((item) => (
+            <button
+              key={item.id}
+              onClick={
+                item.key === "edit"
+                  ? () => {
+                      setFlagText(!flagText);
+                      return () => handleEdit(ticketKey, ticket.id, text);
+                    }
+                  : item.key === "delete"
+                  ? () => handleDelete(ticketKey, ticket.id)
+                  : () => handleMove(ticketKey, item, ticket.id)
+              }
+              className={`px-5 py-1 text-gray-100 bg-blue-500 m-2 rounded-md hover:bg-blue-700 transition-all truncate uppercase text-sm ${
+                item.key === "edit"
+                  ? "bg-gradient-to-r from-yellow-400 to-red-400"
+                  : item.key === "delete"
+                  ? "bg-gradient-to-r from-red-400 to-yellow-400"
+                  : "bg-gradient-to-r from-gray-400 to-blue-400"
+              }`}
+            >
+              {item.todos}
+            </button>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Ticket;
---
+++ src/components/TodoList.js
@@ -0,0 +1,275 @@
+import React, { useEffect, useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "../index.css";
+import Ticket from "./Ticket";
+import swal from "sweetalert";
+
+const TodoList = () => {
+  const [content, setContent] = useState({
+    todos: {
+      key: "todos",
+      tickets: [
+        {
+          id: uuidv4(),
+          text: "Zontrax",
+          createdDate: Date.now(),
+          currentColumnKey: "todos",
+        },
+        {
+          id: uuidv4(),
+          text: "Stronghold",
+          createdDate: Date.now(),
+          currentColumnKey: "todos",
+        },
+        {
+          id: uuidv4(),
+          text: "Viva",
+          createdDate: Date.now(),
+          currentColumnKey: "todos",
+        },
+      ],
+    },
+    inProgress: {
+      key: "in-progress",
+      tickets: [
+        {
+          id: uuidv4(),
+          text: "Zontrax",
+          createdDate: Date.now(),
+          currentColumnKey: "in-progress",
+        },
+        {
+          id: uuidv4(),
+          text: "Tin",
+          createdDate: Date.now(),
+          currentColumnKey: "in-progress",
+        },
+      ],
+    },
+    done: {
+      key: "done",
+      tickets: [
+        {
+          id: uuidv4(),
+          text: "Namfix",
+          createdDate: Date.now(),
+          currentColumnKey: "done",
+        },
+        {
+          id: uuidv4(),
+          text: "Bitwolf",
+          createdDate: Date.now(),
+          currentColumnKey: "done",
+        },
+      ],
+    },
+    needReview: {
+      key: "need-review",
+      tickets: [
+        {
+          id: uuidv4(),
+          text: "Cardguard",
+          createdDate: Date.now(),
+          currentColumnKey: "need-review",
+        },
+        {
+          id: uuidv4(),
+          text: "Trippledex",
+          createdDate: Date.now(),
+          currentColumnKey: "need-review",
+        },
+      ],
+    },
+  });
+
+  const [text, setText] = useState("");
+  const scrollDiv = useRef();
+
+  const handleAddToDo = (e) => {
+    e.preventDefault();
+    if (!text.trim()) {
+      swal("Oops", "Please enter content for ticket!", "error");
+    } else if (text.trim().length < 4) {
+      swal("Oops", " Please enter at least 4 characters!", "error");
+    } else {
+      const objNew = {
+        ...content,
+        todos: {
+          ...content.todos,
+          tickets: [
+            ...content.todos.tickets,
+            {
+              id: uuidv4(),
+              text,
+              createdDate: Date.now(),
+              currentColumnKey: "todos",
+            },
+          ],
+        },
+      };
+      setContent(objNew);
+      swal("Successfull", "Add successfull", "success");
+      scrollDiv.current.scrollIntoView({ behavior: "smooth" });
+      setText("");
+    }
+  };
+
+  const handleMove = (currentColumnkey, targetColumnKey, id) => {
+    const currentTicket = content[currentColumnkey].tickets.filter(
+      (item) => item.id === id
+    );
+
+    const newTickets = content[currentColumnkey].tickets.filter(
+      (item) => item.id !== id
+    );
+
+    setContent({
+      ...content,
+      [currentColumnkey]: { ...content[currentColumnkey], tickets: newTickets },
+      [targetColumnKey.key]: {
+        ...content[targetColumnKey.key],
+        tickets: [
+          ...content[targetColumnKey.key].tickets,
+          { ...currentTicket[0], key: targetColumnKey.todos },
+        ],
+      },
+    });
+  };
+
+  const handleDelete = (currentColumnKey, id) => {
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        const newTickets = content[currentColumnKey].tickets.filter(
+          (item) => item.id !== id
+        );
+        setContent({
+          ...content,
+          [currentColumnKey]: {
+            ...content[currentColumnKey],
+            tickets: newTickets,
+          },
+        });
+      } 
+    });
+  };
+
+  const handleEdit = (currentColumnKey, id, text, e) => {
+    e.preventDefault();
+    const newTickets = content[currentColumnKey].tickets.map((item) =>
+      item.id === id ? { ...item, text,lastModifiedDate:Date.now() } : item
+    );
+    setContent({
+      ...content,
+      [currentColumnKey]: {
+        ...content[currentColumnKey],
+        tickets: newTickets,
+      },
+    });
+  };
+
+  console.log(content);
+
+  return (
+    <div className="bg-[#222222]">
+      <div className="max-w-[1200px] mx-auto flex justify-center items-center flex-col min-h-screen p-5">
+        <div className="bg-[#2a2a2a] p-5 rounded-md mt-5 w-full">
+          <form onSubmit={(e) => handleAddToDo(e)} className="flex flex-col justify-between md:flex-row">
+            <input
+              type="textarea"
+              placeholder="Enter to do..."
+              className="px-5 py-1 bg-transparent border-b w-full md:w-[80%] outline-none text-gray-100 border-blue-400 rounded-md"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="px-5 py-1 mt-3 md:mt-0 rounded-md w-full md:w-[20%] text-gray-100 bg-blue-500 border hover:bg-blue-700 transition-all"
+              onClick={(e) => handleAddToDo(e)}
+            >
+              Add to do
+            </button>
+          </form>
+        </div>
+
+        <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="bg-[#2a2a2a] p-5 rounded-md">
+            <h1 className="under-gra rounded-md text-gray-100 text-xl uppercase">
+              TODO
+            </h1>
+            {content.todos.tickets &&
+              content.todos.tickets.map((item) => (
+                <Ticket
+                  ticket={item}
+                  ticketKey={"todos"}
+                  key={item.id}
+                  handleMove={handleMove}
+                  handleDelete={handleDelete}
+                  handleEdit={handleEdit}
+                />
+              ))}
+          </div>
+
+          <div className="bg-[#2a2a2a] p-5 rounded-md">
+            <h1 className="under-gra rounded-md text-gray-100 text-xl uppercase">
+              In-progress
+            </h1>
+            {content.inProgress.tickets &&
+              content.inProgress.tickets.map((item) => (
+                <Ticket
+                  ticket={item}
+                  ticketKey={"inProgress"}
+                  key={item.id}
+                  handleMove={handleMove}
+                  handleDelete={handleDelete}
+                  handleEdit={handleEdit}
+                />
+              ))}
+          </div>
+
+          <div className="bg-[#2a2a2a] p-5 rounded-md">
+            <h1 className="under-gra rounded-md text-gray-100 text-xl uppercase">
+              Done
+            </h1>
+            {content.done.tickets &&
+              content.done.tickets.map((item) => (
+                <Ticket
+                  ticket={item}
+                  ticketKey={"done"}
+                  key={item.id}
+                  handleMove={handleMove}
+                  handleDelete={handleDelete}
+                  handleEdit={handleEdit}
+                />
+              ))}
+          </div>
+
+          <div className="bg-[#2a2a2a] p-5 rounded-md">
+            <h1 className="under-gra rounded-md text-gray-100 text-xl uppercase">
+              Need-review
+            </h1>
+            {content.needReview.tickets &&
+              content.needReview.tickets.map((item) => (
+                <Ticket
+                  ticket={item}
+                  ticketKey={"needReview"}
+                  key={item.id}
+                  handleMove={handleMove}
+                  handleDelete={handleDelete}
+                  handleEdit={handleEdit}
+                />
+              ))}
+          </div>
+        </div>
+        <div ref={scrollDiv}></div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
